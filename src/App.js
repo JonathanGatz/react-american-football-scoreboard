fe2fdd54ef35fc4a4493e3becf25b43537f39abe
Jonathan Gatz <jonathangatz@Jonathans-MBP.attlocal.net>
2020-07-22 21:10:24 -0400

    Reset Scores To 0

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 //TODO: STEP 1 - Import the useState hook.
-import React from "react";
+import React, {useState} from "react";
 import BottomRow from "./BottomRow";
 import "./App.css";
 
 function App() {
   //TODO: STEP 2 - Establish your applictaion's state with some useState hooks.  You'll need one for the home score and another for the away score.
 
+  const [count, setCount] = useState(0);
+  const [count2, setCount2] = useState(0);
+
   return (
+
     <div className="container">
       <section className="scoreboard">
         <div className="topRow">
@@ -14,12 +18,12 @@ function App() {
             <h2 className="home__name">Lions</h2>
 
             {/* TODO STEP 3 - We need to change the hardcoded values in these divs to accept dynamic values from our state. */}
-            <div className="home__score">32</div>
+            <div className="home__score">{count > 0 ? count : 0}</div>
           </div>
           <div className="timer">00:03</div>
           <div className="away">
             <h2 className="away__name">Tigers</h2>
-            <div className="away__score">32</div>
+            <div className="away__score">{count2 > 0 ? count2 : 0}</div>
           </div>
         </div>
         <BottomRow />
@@ -27,8 +31,9 @@ function App() {
       <section className="buttons">
         <div className="homeButtons">
 
+
           {/* TODO STEP 4 - Now we need to attach our state setter functions to click listeners. */}
-          <button className="homeButtons__touchdown">Home Touchdown</button>
+          <button className onClick={() => setCount(count + 3)}> "homeButtons__touchdown">Home Touchdown</button>
           <button className="homeButtons__fieldGoal">Home Field Goal</button>
         </div>
         <div className="awayButtons">
